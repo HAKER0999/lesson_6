d960d30c06c673c2554ff9d064ebd89d06b02f08
ALIJON0999 <ubaydullayevalijon4@gmail.com>
2023-12-07 15:40:38 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,109 @@
+#
+#
+# print("UYGA VAZIFA!!")
+#
+# a = int(input("Bugun necha kishi bilan suhbaylashdingiz: "))
+# b = str(input("1-suhbatlashgan odamingiz kim edi: "))
+# o = str(input("2-suhbatlashgan odamingiz kim edi: "))
+# l = str(input("3-suhbatlashgan odamingiz kim edi: "))
+# items = [b, o, l]
+#
+# for x in items:
+#     print(items)
+
+
+# k = int(input("qaysi sonni kubini topmoqchisan: "))
+
+
+a = str(input("1-ismni kiriting:"))
+b = str(input("2-ismni kiriting:"))
+c = str(input("3-ismni kiriting:"))
+d = str(input("4-ismni kiriting:"))
+e = str(input("5-ismni kiriting:"))
+
+names = [a, b, c, d, e]
+
+for s in names:
+    print(f"Salom {s}!!!")
+
+
+
+
+# t = int(input("1-sonni kirit: "))
+# r = int(input("2-sonni kirit: "))
+# o = int(input("3-sonni kirit: "))
+# p = int(input("4-sonni kirit: "))
+# k = int(input("5-sonni kirit: "))
+#
+# yogo = [t, r, o, p, k]
+#
+# for z in yogo:
+#     print("sizning javoblaringiz: ", z ** 3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
